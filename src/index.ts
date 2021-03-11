d0a654aaedd8dcc74395035a027b08bf4decefb8
rafaleceu <rafa021dev@gmail.com>
2021-03-11 10:03:58 -0300

    add: basic server start

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import Express from './server/Express';
+
+Express.server.listen(8080, () => console.log(`Servidor rodando na porta 8080`));
---
+++ src/server/Express.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+class Express {
+
+    public server = express();
+
+}
+
+export default new Express();
