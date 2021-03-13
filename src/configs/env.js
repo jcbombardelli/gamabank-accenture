72211bad4104cb94bd48f9f9015c77805e437fdf
Pedro Fires <pedrof.fires@gmail.com>
2021-03-13 16:19:01 -0300

    configurando as .env

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,5 +1,5 @@
 // Hapi
-const dotenv = require('dotenv');
+const dotenv = require('dotenv')
 
 // let path;
 // switch(process.env.NODE_ENV) {
@@ -13,18 +13,23 @@ const dotenv = require('dotenv');
 
 //TODO: Sempre da pra melhorar - Refatorem plmmdss
 dotenv.config({
-    path: process.env.NODE_ENV === 'test' ? '.env.test' : process.env.NODE_ENV === 'development' ? '.env.development' : '.env'
-});
+    path:
+        process.env.NODE_ENV === 'test'
+            ? '.env.test'
+            : process.env.NODE_ENV === 'development'
+            ? '.env.development'
+            : '.env'
+})
 
 module.exports = {
     env: process.env.NODE_ENV,
     secret: process.env.APP_SECRET, //TOKEN JWT
-    database : {
+    database: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         pass: process.env.DB_PASS,
         name: process.env.DB_NAME,
-        port: process.env.DB_PORT,
+        port: process.env.DB_PORT
     },
     salt: process.env.SALT
-};
+}
