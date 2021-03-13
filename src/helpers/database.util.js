a8fe66b05da669721d97974ed8591dc5c32c11cd
unknown <gabrielriosaccioli@gmail.com>
2021-03-13 06:22:05 -0300

    Login

--- src/helpers/database.util.js
+++ src/helpers/database.util.js
@@ -4,10 +4,12 @@ const databaseConfigs  = require('../configs/env').database
 const query = async (sqlstatement) =>{
     const conection = await mysql.createConnection(databaseConfigs)
     return new Promise(async (resolve, reject) =>{
+       
         conection.query(sqlstatement,(err, result) =>{
             if(err) reject(err)
             resolve(result)
             conection.end()
+            
         })
     })
 }
---
+++ src/services/auth.service.js
@@ -0,0 +1,54 @@
+const jwt = require('jsonwebtoken')
+const { secret } = require('../configs/env')
+const userRepository = require('../repository/user.repository')
+const mycripto = require('../helpers/mycripto')
+
+const sign = async (object) => {
+    const token = jwt.sign(object, secret,{
+        algorithm:'HS256',
+        expiresIn: 60
+
+    })
+    return {
+        auth:true,
+        token
+    }
+}
+
+const verify = async (object) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token,secret, (err, decoded) => {
+            if(err) reject ({
+                auth:false,
+                message: 'Falha na autenticação do token'
+            })
+            resolve({
+                auth:true,
+                data: decoded
+            })
+
+        })
+    })
+}
+
+const checkIfExists = async (user) => {
+    return new Promise(async(resolve, reject) => {
+        try {
+            const {userSalt, userPassword} = await  userRepository.getUser(user)
+            const existance = await mycripto.comparePassword(user.password, userSalt, userPassword)
+            resolve(existance)
+
+        }catch(err) {
+            console.error(err)
+            reject(err)
+        }
+
+    })
+}
+
+module.exports = {
+    sign,
+    verify,
+    checkIfExists
+
+}
