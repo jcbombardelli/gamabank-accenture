6ff626074d0e3cf35a8ce83ab0a817bad6a283d9
Pedro Fires <pedrof.fires@gmail.com>
2021-03-13 14:24:00 -0300

    teste novo prettier

--- src/api/services/mail.service.js
+++ src/api/services/mail.service.js
@@ -1,18 +1,18 @@
-const nodemailer = require("nodemailer");
-const configs = require("../../configs/env");
+const nodemailer = require('nodemailer')
+const configs = require('../../configs/env')
 const sendCongratulationsEmail = async () => {
-  let account;
+    let account
 
-  if (configs.env === "test" || configs.env === "development")
-    account = await nodemailer.createTestAccount();
+    if (configs.env === 'test' || configs.env === 'development')
+        account = await nodemailer.createTestAccount()
 
-  const transporter = nodemailer.createTransport({
-    hots: configs.mail.host,
-    port: configs.mail.port,
-    secure: false,
-    auth: {
-      user: account ? account.user : configs.mail.user,
-      pass: "",
-    },
-  });
-};
+    const transporter = nodemailer.createTransport({
+        hots: configs.mail.host,
+        port: configs.mail.port,
+        secure: false,
+        auth: {
+            user: account ? account.user : configs.mail.user,
+            pass: ''
+        }
+    })
+}
