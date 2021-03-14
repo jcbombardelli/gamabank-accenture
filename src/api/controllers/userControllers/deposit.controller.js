7a3b6a09b63bf1ad983506706bac406556633b54
hdbenosx10 <benosx03@gmail.com>
2021-03-14 13:21:31 -0300

    Inicio do endpoint deposit

--- src/api/controllers/userControllers/deposit.controller.js
+++ src/api/controllers/userControllers/deposit.controller.js
@@ -1,8 +1,19 @@
-const depositHandler = (request, h) => {
-    return {
-        status: 'running',
-        timestamp: new Date()
+const validate  = require("../../../helpers/validate")
+const service = require("../../../services/auth.service")
+
+const depositHandler = async (request, h) => {
+    const token = request.headers['x-access-token']
+    try {
+        const result = await service.verify(token)
+        return result
+    }catch(err){
+        const {name,CPF} = request.payload
+            if (name && new validate.ValidaCPF(CPF).valida()) {
+                return `Autorizade Senhore ${name} com o CPF:${CPF}`
+            }
+        return {auth:false, message:`Falha na identificação`}
     }
+    
 }
 
 module.exports = depositHandler
