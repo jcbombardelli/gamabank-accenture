40221131964519d546468ecf7e7972a3a8bde74b
Pedro Fires <pedrof.fires@gmail.com>
2021-03-14 19:44:58 -0300

    Verificaçao de user antes do register

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -1,13 +1,12 @@
 class User {
-
-    constructor({id, username, password, salt}) {
-        this.id = id,
-        this.username = username,
-        this.password = password,
-        this.salt = salt
-
+    constructor({ id, username, password, salt, cpf, name }) {
+        ;(this.id = id),
+            (this.username = username),
+            (this.password = password),
+            (this.salt = salt),
+            (this.cpf = cpf),
+            (this.name = name)
     }
-
 }
 
 module.exports = User
--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -1,15 +1,11 @@
 const repository = require('../repository/user.repository')
 
-
-
-const createAccount = async (newUser) => {
-
-
-    //TODO: Verficar se usuário já existe
-    const result = await repository.save(newUser)
-    return result
-
+const createAccount = async newUser => {
+    const checkUser = await repository.findByUsername(newUser)
+    if (!checkUser) {
+        const result = await repository.save(newUser)
+        return result
+    }
 }
-
 
 module.exports = { createAccount }
