7d7e6bc6569e8c97e6983fab716511a2f1f01f4a
Pedro Fires <pedrof.fires@gmail.com>
2021-03-14 19:44:58 -0300

    Verificaçao de user antes do register

--- src/api/repository/user.repository.js
+++ src/api/repository/user.repository.js
@@ -2,38 +2,36 @@ const database = require('../../helpers/database')
 const crypto = require('../../helpers/mycrypto')
 const User = require('../models/User')
 
-const save = async (user) => {
+const save = async user => {
     return new Promise(async (resolve, reject) => {
         try {
-
             const encrypt = await crypto.encryptPassword(user.password, null)
             user.salt = encrypt.salt
             user.password = encrypt.encryptedPassword
-           
-            const sqlStatement = `INSERT INTO users (username, password, salt) VALUES ("${user.username}", "${user.password}", "${user.salt}");`            
+
+            const sqlStatement = `INSERT INTO users (username, password, salt) VALUES ("${user.username}", "${user.password}", "${user.salt}");`
             const result = await database.execute(sqlStatement)
 
             resolve(result)
         } catch (error) {
             console.error(error)
-            reject(error)      
+            reject(error)
         }
     })
 }
 
-
-const findByUsername = async (username) => {
+const findByUsername = async username => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sqlStatement = `SELECT * FROM users WHERE username="${username}";`            
+            const sqlStatement = `SELECT * FROM users WHERE username="${username}";`
             const result = await database.execute(sqlStatement)
 
             resolve(new User(result))
         } catch (error) {
             console.error(error)
-            reject(error)      
+            reject(error)
         }
     })
 }
 
-module.exports = { save }
+module.exports = { save, findByUsername }
