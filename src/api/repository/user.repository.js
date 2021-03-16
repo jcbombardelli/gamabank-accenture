439f1b5f79b865843c8f04d3a24b90ab577e52b5
Pedro Fires <pedrof.fires@gmail.com>
2021-03-16 18:41:58 -0300

    adicionado function de checar senha

--- src/api/repository/user.repository.js
+++ src/api/repository/user.repository.js
@@ -12,6 +12,7 @@ const save = async user => {
             const id = email + encryptedPassword
 
 <<<<<<< HEAD
+<<<<<<< HEAD
             const sqlStatement = `
             INSERT INTO users (id, name, email, cpf, password, salt)
             VALUES ("${id}", "${name}", "${email}", "${cpf}", "${encryptedPassword}", "${salt}");
@@ -20,6 +21,10 @@ const save = async user => {
             const sqlStatement = `INSERT INTO users (username, password, salt)
             VALUES ("${user.username}", "${user.password}", "${user.salt}");`
 >>>>>>> adicionado um retorno de user n existente
+=======
+            const sqlStatement = `INSERT INTO users (username, password, salt, cpf, name)
+            VALUES ("${user.username}", "${user.password}", "${user.salt}", "${user.cpf}", "${user.name});`
+>>>>>>> adicionado function de checar senha
             const result = await database.execute(sqlStatement)
 
             resolve(result)
@@ -33,12 +38,28 @@ const save = async user => {
 const findByUsername = async username => {
     return new Promise(async (resolve, reject) => {
         try {
-            const sqlStatement = `SELECT * FROM users WHERE cpf="${username}";`
+            const sqlStatement = `SELECT * FROM users WHERE username="${username}";`
             const result = await database.execute(sqlStatement)
 
             resolve(new User(result))
         } catch (error) {
             console.error(error)
+            reject(error)
+        }
+    })
+}
+
+//Função para verificar se a senha possui as regras pela expressão regular em validPassword
+const checkPassword = async senha => {
+    const validPassword = new RegExp('^[a-zA-Z0-9-@#$%&;*]{8,20}$') //Vá mais longe do trello
+    return new Promise(async (resolve, reject) => {
+        try {
+            if (validPassword.test(senha)) {
+                console.log('Password is Valid')
+                resolve(senha)
+            }
+        } catch (error) {
+            console.log(error)
             reject(error)
         }
     })
