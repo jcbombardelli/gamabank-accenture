5dab7d7e84fe50ebb3a61c46c835596bd59806f1
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2021-03-18 21:13:37 -0300

    Criacao UserController

--- src/api/models/User.js
+++ src/api/models/User.js
@@ -4,10 +4,6 @@ class User extends UserController {
     constructor({ id, name, email, cpf, password, salt }) {
         super({ name, email, cpf, password })
         (this.id = id),
-        (this.name = name),
-        (this.email = email),
-        (this.cpf = cpf),
-        (this.password = password),
         (this.salt = salt)
     }
 }
