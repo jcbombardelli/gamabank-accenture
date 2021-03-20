b62d2938edb68a4d0582a0f8cb523c15f0f256e7
Diogo Agra Alexandria <diogoagrasmsdc@gmail.com>
2021-03-20 15:51:28 -0300

    Fluxo do signup refatorado

--- src/api/services/user.service.js
+++ src/api/services/user.service.js
@@ -2,25 +2,31 @@ const repository = require('../repository/user.repository')
 const { encryptPassword } = require('../../helpers/myCrypto')
 const { salt } = require('../../configs/env')
 const { cpfChecker, passwordChecker } = require('../../helpers/recordCheckers')
+const { CustomError } = require('../../helpers/error')
 
 const createUser = async newUser => {
-    const { cpf, password } = newUser
+    try {
+        const { cpf, password } = newUser
 
-    passwordChecker(password)
+        passwordChecker(password)
 
-    cpfChecker(cpf)
+        cpfChecker(cpf)
 
-    const checkUser = await repository.findByCpf(cpf)
+        const checkUser = await repository.findByCpf(cpf)
 
-    if (checkUser.length === 0) {
+        if (checkUser.length > 0) {
+            throw new CustomError({
+                name: 'ErroConflito',
+                message: 'Cpf já cadastrado',
+                statusCode: 409
+            })
+        }
+
         const result = await repository.save(newUser)
         return { status: 'success', message:'Usuário criado com sucesso.', result, code: 201 }
-    } else {
-        throw new CustomError({
-            name: 'ErroConflito',
-            message: 'Cpf já cadastrado',
-            statusCode: 409
-        })
+
+    } catch(err) {
+        throw new CustomError(err)
     }
 }
 
