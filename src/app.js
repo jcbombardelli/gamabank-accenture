88bfb33ed0c66621d50a0d9048020c42161e4e6b
israelblender <israelgomes.prog@gmail.com>
2021-04-08 02:01:37 -0300

    Ajuste variavel ambiente app

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
-const config = require('./configs/env')
-
-console.log(`Execução em ambiente de ${config.env}`);
-
 const server = require("./server");
 server.then((hapi) => {
-    console.log(`NoDesign Server Api on ${config.host}${config.port}`);
+    console.log(`NoDesign Server Api on `);
     hapi.start();
 });
