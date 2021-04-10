74cefaff75161c3626667b7e332626c63a8bf801
anacarolinacv <ana.vasconcelos@ccc.ufcg.edu.br>
2021-04-10 11:51:45 -0300

    alteracoes

--- test/services/debit.service.spec.js
+++ test/services/debit.service.spec.js
@@ -4,46 +4,56 @@ const { getInformationLogin } = require('../../src/api/repositories/login.reposi
 const assert = chai.assert;
 const { updateBalanceAsHolder, updateBalanceAsNotHolder } = require('../../src/api/services/deposit.service')
 
-describe('Validar o service de deposito', async() => {
-    describe('Deposito sendo dono da conta', async () => {
 
-        it('Deve retornar deposito realizada com sucesso', async () => {
-            const deposit = await updateBalanceAsHolder(2, 09857604576, 60);
-            assert.isObject(deposit, {message: 'Depósito realizado com sucesso'});
-        });
+describe('Validar service de debito', async() => {
 
-        it('Deve retornar cpf invalido', async () => {
-            const deposit = await updateBalanceAsHolder(2, 55, 50);
-            assert.equal(deposit, 'Error: CPF inválido');
-        });
-
-        it('Deve retornar que o valor nao pode ser debitado', async () => {
-            const deposit = await updateBalanceAsHolder(2, 09857604576, 0);
-            assert.equal(deposit, 'Error: Valor não pode ser depositado');
-        });
+    before(async () => {
+        await createUser("Ana Carolina", "607.128.880-04", "ana1234", "98925-7834");
     });
 
-    describe('Deposito nao sendo o dono da conta', async () => {
-
-        it('Deve retornar deposito realizada com sucesso', async () => {
-            const debit = await updateBalanceAsNotHolder(40783825099, "carolinavasconcelos90@gmail.com", 60 )
-            assert.isObject(debit, {message: 'Transferência realizada com sucesso'});
+    describe('Validar o service de deposito', async() => {
+        describe('Deposito sendo dono da conta', async () => {
+    
+            it('Deve retornar deposito realizada com sucesso', async () => {
+                const deposit = await updateBalanceAsHolder(2, 09857604576, 60);
+                assert.isObject(deposit, {message: 'Depósito realizado com sucesso'});
+            });
+    
+            it('Deve retornar cpf invalido', async () => {
+                const deposit = await updateBalanceAsHolder(2, 55, 50);
+                assert.equal(deposit, 'Error: CPF inválido');
+            });
+    
+            it('Deve retornar que o valor nao pode ser debitado', async () => {
+                const deposit = await updateBalanceAsHolder(2, 09857604576, 0);
+                assert.equal(deposit, 'Error: Valor não pode ser depositado');
+            });
         });
-
-        it('Deve retornar EMAIL inválido', async () => {
-            const debit = await updateBalanceAsNotHolder(425099, "carogmail.com", 60);
-            assert.equal(debit, 'Error: EMAIL inválido');
-        });
-
-        it('Deve retornar CPF inválido', async () => {
-            const debit = await updateBalanceAsNotHolder(425099, "carolina@gmail.com", 60);
-            assert.equal(debit, 'Error: CPF inválido');
-        });
-
-        it('Deve retornar que o valor nao pode ser debitado', async () => {
-            const debit = await updateBalanceAsNotHolder(40783825099, "carolinavasconcelos90@gmail.com", -2)
-            assert.equal(debit, 'Error: Valor não pode ser depositado');
+    
+        describe('Deposito nao sendo o dono da conta', async () => {
+    
+            it('Deve retornar deposito realizada com sucesso', async () => {
+                const debit = await updateBalanceAsNotHolder(40783825099, "carolinavasconcelos90@gmail.com", 60 )
+                assert.isObject(debit, {message: 'Transferência realizada com sucesso'});
+            });
+    
+            it('Deve retornar EMAIL inválido', async () => {
+                const debit = await updateBalanceAsNotHolder(425099, "carogmail.com", 60);
+                assert.equal(debit, 'Error: EMAIL inválido');
+            });
+    
+            it('Deve retornar CPF inválido', async () => {
+                const debit = await updateBalanceAsNotHolder(425099, "carolina@gmail.com", 60);
+                assert.equal(debit, 'Error: CPF inválido');
+            });
+    
+            it('Deve retornar que o valor nao pode ser debitado', async () => {
+                const debit = await updateBalanceAsNotHolder(40783825099, "carolinavasconcelos90@gmail.com", -2)
+                assert.equal(debit, 'Error: Valor não pode ser depositado');
+            });
+    
         });
 
     });
+
 });
