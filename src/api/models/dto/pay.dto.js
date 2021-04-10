ee4ae3f841cd099ecb92453992f9521021fb09f8
Matheus Gonçalves <matheusgoncalves.noorden@sulamerica.com.br>
2021-04-10 09:31:18 -0300

    auth header

--- src/api/models/dto/pay.dto.js
+++ src/api/models/dto/pay.dto.js
@@ -1,39 +1,40 @@
 const Joi = require("joi");
 
 const BuyDebitRequestDTO = Joi.object({
-
-    cpf: Joi.string().required(),
-    value: Joi.string().required()
-
+  cpf: Joi.string().required(),
+  value: Joi.string().required(),
 }).label("BuyDebitRequestDTO");
 
-const BuyDebitHeaderDTO = Joi.object().keys({
-    'token': Joi.string().required()
-}).options({ allowUnknown: true })
+const BuyDebitHeaderDTO = Joi.object()
+  .keys({
+    Authorization: Joi.string().required(),
+  })
+  .options({ allowUnknown: true });
 
 const BuyDebitResponseDTO = Joi.object({
-    message: Joi.string()
+  message: Joi.string(),
 }).label("BuyDebitResponseDTO");
 
 const BuyCreditRequestDTO = Joi.object({
-    value: Joi.string().required(),
-    installment: Joi.number().required()
+  value: Joi.string().required(),
+  installment: Joi.number().required(),
 }).label("BuyCreditRequestDTO");
 
-const BuyCreditHeaderDTO = Joi.object().keys({
-    'token': Joi.string().required()
-}).options({ allowUnknown: true })
+const BuyCreditHeaderDTO = Joi.object()
+  .keys({
+    Authorization: Joi.string().required(),
+  })
+  .options({ allowUnknown: true });
 
 const BuyCreditResponseDTO = Joi.object({
-    message: Joi.string()
+  message: Joi.string(),
 }).label("BuyCreditResponseDTO");
 
-
 module.exports = {
-    BuyDebitRequestDTO,
-    BuyDebitHeaderDTO,
-    BuyDebitResponseDTO,
-    BuyCreditRequestDTO,
-    BuyCreditHeaderDTO,
-    BuyCreditResponseDTO
-  }
+  BuyDebitRequestDTO,
+  BuyDebitHeaderDTO,
+  BuyDebitResponseDTO,
+  BuyCreditRequestDTO,
+  BuyCreditHeaderDTO,
+  BuyCreditResponseDTO,
+};
